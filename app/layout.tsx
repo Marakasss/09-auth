4c3ed0f41b1bf6475daa3942a2f5c9ca9fa22aaa
ígorpetriv.bsc <igorpetriv.bsc@gmail.com>
2025-07-12 23:38:26 +0300

    provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import AuthProvider from "@/components/AuthPorovider/AuthProvider";
+import AuthProvider from "@/components/AuthProvider/AuthProvider";
 
 //Fonts----------------------------------------
 
